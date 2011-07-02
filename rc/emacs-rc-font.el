5ae5220fb8e7b7ec25c1e295ce14be32b0f03e37
Yegor Bayev <kodxxx@gmail.com>
2011-07-02 05:09:26 +0400

    add new font-face to config, see README

--- rc/emacs-rc-font.el
+++ rc/emacs-rc-font.el
@@ -5,7 +5,8 @@
 (cond (window-system
     ;; (set-face-font 'default "-outline-Consolas-normal-r-normal-*-18-112-96-96-c-*-*")
     ;; (set-face-font 'default "Terminus-12")
-    (set-face-font 'default "DejaVu Sans Mono 11")
+    ;;(set-face-font 'default "DejaVu Sans Mono 11")
+    (set-face-font 'default "Meslo LG S 11")
 ))
 
 ;;; emacs-rc-font.el ends here
