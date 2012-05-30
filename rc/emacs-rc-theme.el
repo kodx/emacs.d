e9cc598eec0ca1c10275a324ae1588947374e7f4
Yegor Bayev <kodxxx@gmail.com>
2012-05-30 22:26:35 +0400

    Added color-theme plugin to make emacs standalone

--- rc/emacs-rc-theme.el
+++ rc/emacs-rc-theme.el
@@ -1,3 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/plugins/color-theme")
+(require 'color-theme)
 (add-to-list 'load-path "~/.emacs.d/themes")
 (load-theme 'tangotangokx)
 (color-theme-tangotangokx)
