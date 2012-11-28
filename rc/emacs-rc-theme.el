bfcfc57754863034146c6cfef6f190f7e49fd7c7
Yegor Bayev <kodxxx@gmail.com>
2012-11-28 14:35:01 +0400

    color theme updated and fixed

--- rc/emacs-rc-theme.el
+++ rc/emacs-rc-theme.el
@@ -1,6 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/color-theme")
 (require 'color-theme)
 (add-to-list 'load-path "~/.emacs.d/themes")
+
 (load-theme 'tangotangokx)
-(color-theme-tangotangokx)
 
